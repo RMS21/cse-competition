e069087df56ea7d7199b5d8711f15e7b14d4d282
Kamran Abdi <kamranabdicse@gmail.com>
2017-02-27 14:06:40 +0330

    Adding login page

--- resources/views/public/login.blade.php
+++ resources/views/public/login.blade.php
@@ -1,42 +1,75 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>login</title>
-    <style media="screen">
-      form{
-        width: 400px;
-        margin: 300px auto;
-        padding: 10px;
-        font-size: 25px;
-        text-align: right;
-      }
-      label{
-        padding: 5px;
-        margin: 0 10px;
-        width: 100px;
-      }
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width , initial-scale=1">
+        <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="Material-Kit/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.css">
 
-      input{
-        margin: 10px 0;
-        width: 200px;
-      }
+        <link rel="stylesheet" href="Material-Kit/assets/css/material-kit.css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+        
+        
+        <link rel="stylesheet" href="assets/css/login.css">
+        <title>مسابقه</title>
+    
+    <style> 
     </style>
-  </head>
-  <body>
-    @if(count($errors) > 0)
-      @foreach($errors->all() as $error)
-        {{ $error }}
-      @endforeach
-    @endif
-    <form action="{{ route('post_team_login') }}" method="post">
-        <label>TeamName</label>
-        <input type="text" name="name">
-        <br>
-        <label>Password</label>
-        <input type="password" name="password">
-        <button type="submit" name="submit">Submit</button>
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    </form>
-  </body>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <div class="card card-signup">
+                        <form>
+                            <div class="header header-primary text-center">
+                                <h1>ورود به سایت</h1>
+                            </div>
+                            <div class="title-login">
+                                <img src="assets/img/logo.png" alt="login">
+                            </div>
+                            <div class="content">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><img src="assets/img/glyphicons-group.png" alt=""></span>
+                                    <div class="form-group label-floating has-error">
+                                        <label class="control-label">نام گروه وارد نشده</label>
+                                        <input type="text" class="form-control" placeholder="نام گروه">
+                                        <span class="material-icons form-control-feedback">clear</span>
+                                        <span class="material-input"></span>
+                                    </div>
+                                </div>
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <div class="form-group label-floating has-error">
+                                        <label class="control-label">رمز عبور وارد نشده</label>
+                                        <input type="text" class="form-control" placeholder="رمز عبور">
+                                        <span class="material-icons form-control-feedback">clear</span>
+                                        <span class="material-input"></span>
+                                    </div>
+                                </div>
+                                <div class="field-checkbox">
+                                    <div class="checkbox-inline">
+                                        <label>من را به خاطر بسپار<input type="checkbox"></label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="login" class="sr-only">Login</label>
+                                    <input type="submit" class="form-control" id="login" value="ورود به سایت">
+                                </div>
+                            </div>    
+                        </form>
+                </div>
+            </div>
+        </div>
+    
+    <script src="E:\web\bootstrap-3.3.7-dist\jquery-3.1.1.min.js"></script>
+    <script src="E:\web\bootstrap-3.3.7-dist\js\bootstrap.js"></script>
+    
+    <script src="Material-Kit/assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="Material-Kit/assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="Material-Kit/assets/js/material.min.js"></script>
+    
+    </body>
 </html>
