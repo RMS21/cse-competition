013b81e7e1f9c6333f26dbfd8e8f8de13dd0f1b8
Kamran Abdi <kamranabdicse@gmail.com>
2017-02-28 13:47:03 +0330

    submit profile

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,1 +1,198 @@
-Home
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width , initial-scale=1">
+        <link rel="stylesheet" href="E:\web\bootstrap-3.3.7-dist\css\bootstrap.css">
+        <link rel="stylesheet" href="../bootstrap-rtl/css/bootstrap.rtl.css">
+
+        <link rel="stylesheet" href="Material-Kit/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../bootstrap-rtl/css/bootstrap.rtl.min.css">
+        <link rel="stylesheet" href="../font/font-awesome-4.7.0/font-awesome.min.css">
+
+        <link rel="stylesheet" href="Material-Kit/assets/css/material-kit.css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+        <link rel="stylesheet" href="../font/font-awesome-4.7.0/font-awesome.css">
+        <link rel="stylesheet" href="assets/css/profile.css">
+        
+        <title>مسابقه</title>
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row navbar-information">
+                <div class="col-md-4">
+                    <div class="group-information">
+                        <span class="group-property">گروه</span>
+                        <span class="value-property">تناننتاننتمنتمنساب ساهارم با سیمنتن</span>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="group-information">
+                        <span class="group-property">امتیاز</span>
+                        <span class="value-property">98798</span>
+                    </div>
+                </div>
+                <div class="col-md-1">
+                    <div class="group-information">
+                        <span class="group-property">سطح </span>
+                        <span class="value-property">1</span>
+                    </div> 
+                </div>
+                <div class="col-md-2">
+                    <div class="group-information">
+                        <span class="group-property">تعداد اعضا</span>
+                        <span class="value-property">3</span>
+                    </div>
+                </div>
+                <div class="col-md-offset-2 col-md-1 logo">
+                    <img src="assets/img/logo1.png" alt="logo">
+                </div> 
+            </div>
+            <div class="container">
+                <div class="row bs-wizard" style="border-bottom:0;">
+                    <div class="col-xs-3 bs-wizard-step complete">
+                        <div class="text-center bs-wizard-stepnum">سطح 1</div>
+                        <div class="progress"><div class="progress-bar"></div></div>
+                        <a href="#" class="bs-wizard-dot"></a>
+                        <div class="bs-wizard-info text-center">برنامه نویسی</div>
+                    </div>
+                    
+                    <div class="col-xs-3 bs-wizard-step complete"><!-- complete -->
+                        <div class="text-center bs-wizard-stepnum">سطح 2</div>
+                        <div class="progress"><div class="progress-bar"></div></div>
+                        <a href="#" class="bs-wizard-dot"></a>
+                        <div class="bs-wizard-info text-center">تحلیل مدار</div>
+                    </div>
+                    
+                    <div class="col-xs-3 bs-wizard-step active"><!-- complete -->
+                        <div class="text-center bs-wizard-stepnum">سطح 3</div>
+                        <div class="progress"><div class="progress-bar"></div></div>
+                        <a href="#" class="bs-wizard-dot"></a>
+                        <div class="bs-wizard-info text-center">مدار منطقی</div>
+                    </div>
+                    
+                    <div class="col-xs-3 bs-wizard-step disabled">
+                        <div class="text-center bs-wizard-stepnum">سطح 4</div>
+                        <div class="progress"><div class="progress-bar"></div></div>
+                        <a href="#" class="bs-wizard-dot"></a>
+                        <div class="bs-wizard-info text-center">خلاقیت</div>
+                    </div>
+                </div>
+        
+	        </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="form buy-question">
+                            <div class="header header-primary text-center">
+                                <span>هشت وزیر</span>
+                            </div>
+                            <div class="content">
+                                <div class="level">
+                                    <span>سطح :</span>
+                                    <span>آسان</span>
+                                </div>
+                                <div class="point">
+                                    <span>امتیاز : </span>
+                                    <span>60</span>
+                                </div>
+                                <div>
+                                    <span>وضعیت سوال : </span>
+                                    <span>درست</span>
+                                    <i class="glyphicon glyphicon-ok"></i>
+                                </div>
+                            </div>
+                            <button class="btn btn-default">خرید</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="form buy-question">
+                            <div class="header header-primary text-center">
+                                <span>مدار میلی</span>
+                            </div>
+                            <div class="content">
+                                <div class="level">
+                                    <span>سطح :</span>
+                                    <span>آسان</span>
+                                </div>
+                                <div class="point">
+                                    <span>امتیاز : </span>
+                                    <span>60</span>
+                                </div>
+                                <div>
+                                    <span>وضعیت سوال : </span>
+                                    <span>جواب داده نشده</span>
+                                    <i class="glyphicon glyphicon-pencil"></i>
+                                </div>
+                            </div>
+                            <button class="btn btn-default">خرید</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="form buy-question">
+                            <div class="header header-primary text-center">
+                                <span>ضرب کننده</span>
+                            </div>
+                            <div class="content">
+                                <div class="level">
+                                    <span>سطح :</span>
+                                    <span>آسان</span>
+                                </div>
+                                <div class="point">
+                                    <span>امتیاز : </span>
+                                    <span>60</span>
+                                </div>
+                                <div>
+                                    <span>وضعیت سوال : </span>
+                                    <span>غلط</span>
+                                    <i class="glyphicon glyphicon-remove"></i>
+                                </div>
+                            </div>
+                            <button class="btn btn-default">خرید</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="form buy-question">
+                            <div class="header header-primary text-center">
+                                <span>سوال</span>
+                            </div>
+                            <div class="content">
+                                <div class="level">
+                                    <span>سطح :</span>
+                                    <span>آسان</span>
+                                </div>
+                                <div class="point">
+                                    <span>امتیاز : </span>
+                                    <span>60</span>
+                                </div>
+                                <div>
+                                    <span>وضعیت سوال : </span>
+                                    <span>در حال بررسی</span>
+                                    <i class="glyphicon glyphicon-retweet"></i>
+                                </div>
+                            </div>
+                            <button class="btn btn-default">خرید</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <script src="E:\web\bootstrap-3.3.7-dist\jquery-3.1.1.min.js"></script>
+        <script src="E:\web\bootstrap-3.3.7-dist\js\bootstrap.js"></script>
+
+        
+        <script src="Material-Kit/assets/js/jquery.min.js" type="text/javascript"></script>
+        <script src="Material-Kit/assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="Material-Kit/assets/js/material.min.js"></script>
+    </body>
+</html>
