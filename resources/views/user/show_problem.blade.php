d2706994756310ae9c793f63a47e23044761479b
Kamran Abdi <kamranabdicse@gmail.com>
2017-03-01 13:54:52 +0330

    question

---
+++ resources/views/user/show_problem.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width , initial-scale=1">
+        <link rel="stylesheet" href="E:\web\bootstrap-3.3.7-dist\css\bootstrap.css">
+        <link rel="stylesheet" href="../bootstrap-rtl/css/bootstrap.rtl.css">
+
+        <link rel="stylesheet" href="Material-Kit/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../bootstrap-rtl/css/bootstrap.rtl.min.css">
+        
+        <link rel="stylesheet" href="Material-Kit/assets/css/material-kit.css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+        <link rel="stylesheet" href="../font/font-awesome-4.7.0/font-awesome.css">
+        <link rel="stylesheet" href="../font/font-awesome-4.7.0/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/question.css">
+        <title>مسابقه</title>
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row navbar-information">
+                <div class="col-md-4">
+                    <div class="group-information">
+                        <span class="group-property">گروه</span>
+                        <span class="value-property">تناننتاننتمنتمنساب ساهارم با سیمنتن</span>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="group-information">
+                        <span class="group-property">امتیاز</span>
+                        <span class="value-property">98798</span>
+                    </div>
+                </div>
+                <div class="col-md-1">
+                    <div class="group-information">
+                        <span class="group-property">سطح </span>
+                        <span class="value-property">1</span>
+                    </div> 
+                </div>
+                <div class="col-md-2">
+                    <div class="group-information">
+                        <span class="group-property">تعداد اعضا</span>
+                        <span class="value-property">3</span>
+                    </div>
+                </div>
+                <div class="col-md-offset-2 col-md-1 logo">
+                    <img src="assets/img/logo1.png" alt="logo">
+                </div> 
+            </div>
+            <div class="container question">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="card card-signup">
+                                <div class="header header-primary text-center">
+                                    <h1>سوال شیسبسیبسب</h1>
+                                    <h2>
+                                        <span>امتیاز :</span>
+                                        <span>60</span>
+                                    </h2>
+                                    <h3>
+                                        <span>سطح :</span>
+                                        <span>1</span>
+                                    </h3>
+                                </div>
+                                <div class="content">
+                                    <p>
+                                        شسیب شسیبنس سیبنتمش سیبنمتمشسیبتمنتیبمنشسبتمنتسییبمتتسبنمتسبمنن
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسش
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبسبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+شیبشسیبسشیبسشیبشسیبسیبسیبسبسشیبسیبسشبسبسبسشیبسشبس
+                                    </p>
+                                    <!--<img src="assets/img/logo.png" alt="">-->
+                                </div>
+                                <hr>
+                                <form action="" class="form-inline">
+                                    <div class="form-group">
+                                        <input type="submit"  class="form-control" name="submit" value="درخواست بازبینی">    
+                                        <input type="submit" class="form-control" name="cancel" value="انصراف">    
+                                    </div>        
+                                </form>    
+                            
+                        </div>
+                    </div>
+                </div>                    
+            </div>    
+        </div>
+    </body>
+    <script src="E:\web\bootstrap-3.3.7-dist\jquery-3.1.1.min.js"></script>
+    <script src="E:\web\bootstrap-3.3.7-dist\js\bootstrap.js"></script>
+    
+    <script src="Material-Kit/assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="Material-Kit/assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="Material-Kit/assets/js/material.min.js"></script>
+    
+</html>
--- resources/views/user/waiting.blade.php
+++ resources/views/user/waiting.blade.php
@@ -1,10 +1,76 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Waiting</title>
-  </head>
-  <body style="text-align: center;">
-      <h1>waiting</h1>
-  </body>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width , initial-scale=1">
+        <link rel="stylesheet" href="E:\web\bootstrap-3.3.7-dist\css\bootstrap.css">
+        <link rel="stylesheet" href="../bootstrap-rtl/css/bootstrap.rtl.css">
+
+        <link rel="stylesheet" href="Material-Kit/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../bootstrap-rtl/css/bootstrap.rtl.min.css">
+        
+        <link rel="stylesheet" href="Material-Kit/assets/css/material-kit.css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+        <link rel="stylesheet" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.css">
+        <link rel="stylesheet" href="assets/fonts/font-awesome-4.7.0/fonts/fontawesome-webfont.ttf">
+
+        
+
+        <link rel="stylesheet" href="assets/css/check.css">
+        <title>مسابقه</title>
+        <style>
+        
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row navbar-information">
+                <div class="col-md-1 logo">
+                    <img src="assets/img/logo1.png" alt="logo">
+                </div>
+                <div class="col-md-3">
+                    <div class="group-information">
+                        <span class="group-property">گروه :</span>
+                        <span class="value-property">تناننتاننتمنتمنساب ساهارم با سیمنتن</span>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="group-information">
+                        <span class="group-property">امتیاز :</span>
+                        <span class="value-property">98798</span>
+                    </div>
+                </div>
+                <div class="col-md-1">
+                    <div class="group-information">
+                        <span class="group-property">سطح :</span>
+                        <span class="value-property">1</span>
+                    </div> 
+                </div>
+                <div class="col-md-2">
+                    <div class="group-information">
+                        <span class="group-property">تعداد اعضا :</span>
+                        <span class="value-property">3</span>
+                    </div>
+                </div>
+                <div class="col-md-1 col-md-offset-2">
+                    <a href="" data-toggle="modal" data-target="#logout">
+                        <span class="glyphicon glyphicon-off"></span>
+                    </a>
+                </div> 
+            </div>
+            <div class="row content">
+                <div class="col-md-3 col-md-offset-5">
+                    <span>لطفا منتظر بمانید...</span>
+                    <span class="spinner"><i class="fa fa-refresh fa-spin"></i></span>
+                </div>
+            </div>
+        </div>
+    <script src="E:\web\bootstrap-3.3.7-dist\jquery-3.1.1.min.js"></script>
+    <script src="E:\web\bootstrap-3.3.7-dist\js\bootstrap.js"></script>
+    
+    <script src="Material-Kit/assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="Material-Kit/assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="Material-Kit/assets/js/material.min.js"></script>
+    
+    </body>
 </html>
